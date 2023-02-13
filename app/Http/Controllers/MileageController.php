4dfe84d78f36c151e94cf5edcb3f7bd091f78cea
unknown <dgloria.web@gmail.com>
2023-02-13 07:00:51 +0000

    added mileages controller and database migration, also vue frontend page

---
+++ app/Http/Controllers/MileageController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mileage;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class MileageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Inertia::render('Mileages/Index', [
+            //
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Mileage  $mileage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mileage $mileage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Mileage  $mileage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mileage $mileage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Mileage  $mileage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Mileage $mileage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Mileage  $mileage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mileage $mileage)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_02_13_063527_create_mileages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\DBAL\TimestampType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mileages', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->mediumInteger('mileage')->nullable();
+            $table->smallInteger('partner_id');
+            $table->smallInteger('location_id_start');
+            $table->smallInteger('location_id_end');
+            $table->boolean('is_personal');
+            $table->string('comments')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mileages');
+    }
+};
